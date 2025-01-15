6c024a27391b5bfcbc42bf66e790609a85ed6f56
Greap <3863749431@qq.com>
2025-01-15 22:02:33 +0800

    Site updated: 2025-01-15 22:02:31

--- js/welcome.js
+++ js/welcome.js
@@ -1,17 +1,7 @@
-function welcome(){
-    let welcome_text = '欢迎光顾本蒟蒻的小窝~'
-    if(document.referrer!==''){
-        let referrer=document.referrer.split("/")[2];
-        welcome_text="欢迎你，来自"+referrer.toUpperCase()+"的用户！";
-        if(referrer.toUpperCase()==document.domain.toUpperCase())return;
-    }
-    swal({
-        title: " 欢迎！",
-        text: welcome_text+'\n打开页面下方音乐以获得更佳体验！',
-        timer: 3000,
-        showConfirmButton: false
-    });
-}
-$(document).ready(()=>{
-    welcome()
-})
+Swal.fire({
+    title: '欢迎光顾本葡萄的小窝⌯ᐢᗜᐢ⌯ಣ',
+    html: '来了就收藏一下呗~<br>搭配页面左下方音乐食用体验更佳哦~<br>右上角可以进行全局搜索~<br>如果对文章内容有什么问题或者有什么想说的，可以在下方留言，我会尽快回复的~',
+    iconHtml: '<i class="fas fa-home"></i>',
+    confirmButtonText: '我知道啦',
+    footer: '<a href="/2023/09/24/hexoaddsweetalert2/"><i class="fas fa-link"></i>你也想要同款弹窗？点击这里试试！</a>'
+  })
